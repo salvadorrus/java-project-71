2201be25913144af653f5651e06d984c22d94d59
salvadorrus <ntvkras@list.ru>
2024-07-18 15:34:27 +0300

    Add picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "Demo", mixinStandardHelpOptions = true)
+public class App implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path of first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath2", description = "path of second file")
+    private String filepath2;
+
+    @CommandLine.Option(
+            names = {"-f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [default: stylish]"
+    )
+    private String format;
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
