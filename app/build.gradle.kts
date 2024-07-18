a1143e7ac3b3def1eee1914243bda07280cff80d
salvadorrus <ntvkras@list.ru>
2024-07-18 11:52:31 +0300

    Init commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    //id("java")
+    application
+}
+
+//application {
+//    mainClass = "hexlet.code.App"
+//}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
