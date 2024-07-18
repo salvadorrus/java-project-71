c3014b05f0ae2ef9cb0bb38becc198de8368f58d
salvadorrus <ntvkras@list.ru>
2024-07-19 00:17:18 +0300

    Fix Comparator.

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -6,7 +6,7 @@ import java.util.TreeSet;
 
 public class Comparator {
 
-    public static List<Map<String, Object>> compare(Map<String, Object> file1, Map<String ,Object> file2) {
+    public static List<Map<String, Object>> compare(Map<String, Object> file1, Map<String, Object> file2) {
         var keys = new TreeSet<>();
         keys.addAll(file1.keySet());
         keys.addAll(file2.keySet());
