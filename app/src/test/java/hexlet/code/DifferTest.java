6c390092b2c6d668f64a936473534f70cf7447d2
salvadorrus <ntvkras@list.ru>
2024-07-30 15:37:47 +0300

    Fix DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
@@ -15,23 +16,27 @@ class DifferTest {
     private static String resultPlain;
     private static String resultStylish;
 
-    private static String getPath(String filePath) throws Exception {
-        var readPath = Paths.get("src/test/resources", filePath).toAbsolutePath().normalize();
-        return Files.readString(readPath).trim();
+    private static Path getPath(String filePath) {
+        return Paths.get("src/test/resources", filePath).toAbsolutePath().normalize();
+        //return Files.readString(readPath).trim();
+    }
+
+    private  static String getRead(String filePath) throws Exception {
+        return Files.readString(getPath(filePath)).trim();
     }
 
     @BeforeAll
     public static void beforeAll() throws Exception {
-        resultJson = getPath("resultJson.json");
-        resultPlain = getPath("resultPlain.txt");
-        resultStylish = getPath("resultStylish.txt");
+        resultJson = getRead("resultJson.json");
+        resultPlain = getRead("resultPlain.txt");
+        resultStylish = getRead("resultStylish.txt");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yml"})
     public void diffTest(String format) throws Exception {
-        var resultPath1 = getPath("filepath1." + format);
-        var resultPath2 = getPath("filepath2." + format);
+        var resultPath1 = getPath("filepath1." + format).toString();
+        var resultPath2 = getPath("filepath2." + format).toString();
 
         assertEquals(Differ.generate(resultPath1, resultPath2), resultStylish);
         assertEquals(Differ.generate(resultPath1, resultPath2, "json"), resultJson);
